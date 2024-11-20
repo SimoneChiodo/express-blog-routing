dc8ee5f7c94246afd0cd88078dfea54a989fe0c2
SimoneChiodo <simochi.005@gmail.com>
2024-11-20 17:06:47 +0100

    add post-printing inside the crud-methods (bonus)

--- router/posts.js
+++ router/posts.js
@@ -1,34 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
+// Importo i post
+const posts = require("../db/posts");
+
 // METODI CRUD
 // Metodo: Index (Visualizzare tutti gli elementi)
 router.get("/", (req, res) => {
-    res.send(`Visualizzazione dei post`);
+    res.type("json").send({
+        msg: "Visualizzazione dei post",
+        posts,
+    });
 });
+
 // Metodo: Show (Visualizzare un elemento)
 router.get("/:id", (req, res) => {
-    const { id } = req.params;
-    res.send(`Visualizzazione del post ${id}`);
+    const id = parseInt(req.params.id);
+    console.log(posts[id - 1]);
+
+    // Controllo l'ID
+    if (id < 1 || id > posts.length || isNaN(id)) {
+        res.status(400).json({
+            error: "Invalid ID",
+        });
+
+        return;
+    }
+
+    res.json({
+        msg: "Visualizzazione del post",
+        post: posts[id - 1],
+    });
 });
+
 // Metodo: Store (Creare un nuovo elemento)
 router.post("/", (req, res) => {
-    res.send(`Creazione di un nuovo post`);
+    res.type("json").send(`Creazione di un nuovo post`);
 });
+
 // Metodo: Update (Modificare interamente un elemento)
 router.put("/:id", (req, res) => {
     const { id } = req.params;
-    res.send(`Modifica integrale del post ${id}`);
+    res.type("json").send(`Modifica integrale del post ${id}`);
 });
+
 // Metodo: Modify (Modificare parzialmente un elemento)
 router.patch("/:id", (req, res) => {
     const { id } = req.params;
-    res.send(`Modifica parziale del post ${id}`);
+    res.type("json").send(`Modifica parziale del post ${id}`);
 });
+
 // Metodo: Destroy (Eliminare un elemento)
 router.delete("/:id", (req, res) => {
     const { id } = req.params;
-    res.send(`Eliminazione del post ${id}`);
+    res.type("json").send(`Eliminazione del post ${id}`);
 });
 
 //Esporto i metodi
