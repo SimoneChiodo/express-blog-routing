3a567a5bb2842dfc722b03a8163d2ea1090bf09c
SimoneChiodo <simochi.005@gmail.com>
2024-11-20 16:00:27 +0100

    add router; crud-methods

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ const port = 3000;
 // Abilito assets statici
 app.use(express.static("public"));
 
-// Pagina: Home
-app.get("/", (req, res) => {
-    res.type("html").send("Server del mio blog");
-});
+// Importo il router
+const postsRouter = require("./router/posts");
+// Imposto l'uso delle rotte contenute nel router
+app.use("/posts", postsRouter);
 
-// Pagina: Bacheca
-app.get("/bacheca", (req, res) => {
-    res.type("json").send({
-        totalePost: posts.length,
-        listaPost: posts,
-    });
-});
+// // Pagina: Home
+// app.get("/", (req, res) => {
+//     res.type("html").send("Server del mio blog");
+// });
 
+// // Pagina: Bacheca
+// app.get("/bacheca", (req, res) => {
+//     res.type("json").send({
+//         totalePost: posts.length,
+//         listaPost: posts,
+//     });
+// });
+
+// Avvio
 app.listen(port, () => {
     console.log(`App Express listening on port ${port}`);
 });
@@ -60,36 +66,3 @@ let posts = [
         tag: ["torta", "dolce"],
     },
 ];
-console.log(posts);
-
-// REMOVED: CREAZIONE DINAMICA DI POST
-//
-//   ||
-//   ||
-//  \  /
-//   \/
-//
-// // Dichiarazione oggetto Post
-// let post = {
-//     titolo: "",
-//     contenuto: "",
-//     immagine: "",
-//     tag: [],
-// };
-
-// // Array di post
-// let posts = [];
-// for (let i = 0; i < 5; i++) {
-//     addObjectToArray(posts, post);
-// }
-// posts[0].immagine = "./images/ciambellone.jpeg";
-
-// /**
-//  * Funzione che aggiunge un oggetto a un array un oggetti.
-//  *
-//  * @param {Array} array array a cui aggiugere l'oggetto
-//  * @param {Object} object oggetto da aggiungere
-//  */
-// function addObjectToArray(array, object) {
-//     array.push({ ...object });
-// }
---
+++ router/posts.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+// METODI CRUD
+// Metodo: Index (Visualizzare tutti gli elementi)
+router.get("/", (req, res) => {
+    res.send(`Visualizzazione dei post`);
+});
+// Metodo: Show (Visualizzare un elemento)
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Visualizzazione del post ${id}`);
+});
+// Metodo: Store (Creare un nuovo elemento)
+router.post("/", (req, res) => {
+    res.send(`Creazione di un nuovo post`);
+});
+// Metodo: Update (Modificare interamente un elemento)
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Modifica integrale del post ${id}`);
+});
+// Metodo: Modify (Modificare parzialmente un elemento)
+router.patch("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Modifica parziale del post ${id}`);
+});
+// Metodo: Destroy (Eliminare un elemento)
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Eliminazione del post ${id}`);
+});
+
+module.exports = router;
